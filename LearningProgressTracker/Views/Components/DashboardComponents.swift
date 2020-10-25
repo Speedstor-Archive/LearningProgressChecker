f5e3822f8ad1580c238e4e41ada989273ff654e1
Speedstor <razer.theflash@gmail.com>
2020-10-24 23:31:09 -0700

    first commit

---
+++ LearningProgressTracker/Views/Components/DashboardComponents.swift
@@ -0,0 +1,21 @@
+//
+//  DashboardComponents.swift
+//  LearningProgressTracker
+//
+//  Created by Yan Shing Cheung on 7/17/20.
+//  Copyright © 2020 Yan Shing Cheung. All rights reserved.
+//
+
+import SwiftUI
+
+struct DashboardComponents: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DashboardComponents_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardComponents()
+    }
+}
---
+++ LearningProgressTracker/Views/InputView.swift
@@ -0,0 +1,21 @@
+//
+//  InputView.swift
+//  LearningProgressTracker
+//
+//  Created by Yan Shing Cheung on 7/17/20.
+//  Copyright © 2020 Yan Shing Cheung. All rights reserved.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct InputView_Previews: PreviewProvider {
+    static var previews: some View {
+        InputView()
+    }
+}
